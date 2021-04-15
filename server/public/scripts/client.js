cb53f9a1c12243ac2656f178d765601a69d1ae23
Alex White <alexlloydwhite@gmail.com>
2021-04-15 17:04:04 -0500

    setting up server logic

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,18 +2,31 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!");
-  $('#submitGuess').on('click', displayResults);
+  $('#input').on('click', '#submitGuess', displayResults);
+  $('#input').on('click', '#restart', restart)
+}
+
+let totalGuess = 0
+
+function restart() {
+  console.log('clicked restart');
+  // TODO - ask the server for a new randomNumber
+
+  // reappend the inputs...
+  $('#input').empty().append(`<input id="guess1" type="text" placeholder="Player 1 Guess" />
+  <input id="guess2" type="text" placeholder="Player 2 Guess" />
+  <button id="submitGuess">Submit Guess</button>`);
 }
 
 function displayResults(){
-  // assign value of guess inputs to variable
+  // TODO = send (POST) guesses to server to compare against randomNumber
   let guess1 = $('#guess1').val();
   let guess2 = $('#guess2').val();
   
-  let randomNumber = 5;
+  totalGuess ++;
+  $('#totalGuessOut').empty().append(totalGuess);
   
-  console.log('click!', guess1);
-  
+  // player 1 logic
   if( guess1 > randomNumber ) {
     $('#history').append(`<li>Player 1 guess of ${guess1} was too high!!</li>`);
   } else if (guess1 < randomNumber ) {
@@ -21,11 +34,20 @@ function displayResults(){
   } else if ( guess1 = randomNumber ) {
     $('#history').append(`<li>Player 1 guess of ${guess1} was CORRECT!!!!</li>`);
     alert('WAY TO GO!! PLAYER 1 GUESSED RIGHT!!!')
+    $('#input').empty().append(`<button id="restart">Restart?</button>`)
   }
 
+  // player 2 logic
+  if( guess2 > randomNumber ) {
+    $('#history').append(`<li>Player 2 guess of ${guess2} was too high!!</li>`);
+  } else if (guess2 < randomNumber ) {
+    $('#history').append(`<li>Player 2 guess of ${guess1} was too low!!</li>`);
+  } else if ( guess2 = randomNumber ) {
+    $('#history').append(`<li>Player 2 guess of ${guess2} was CORRECT!!!!</li>`);
+    alert('WAY TO GO!! PLAYER 2 GUESSED RIGHT!!!')
+    $('#input').empty().append(`<button id="restart">Restart?</button>`)   }
 
-  // logic to execute with value
-
-
-
+  // clear inputs
+  $('#guess1').val('');
+  $('#guess2').val('');
 }
--- server/server.js
+++ server/server.js
@@ -10,12 +10,44 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 // GET & POST Routes go here
+// guesses sent to client
+app.get('/guesses', (req, res) => {
+    res.send(guesses)
+    console.log('Guess sent to browser', guesses)
+})
+
+// guesses sent to the server
+app.post('/guesses', (req, res) => {
+    let newGuess = req.body;
+    console.log('Got a new guess', newGuess);
+
+    checkGuess(guesses);
+    res.sendStatus(201);
+
+})
+
+function checkGuess(){
+  //check the guess against randomNumber
+}
 
 let randomNumber = (min,max) => {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-console.log('random number:', randomNumber(1,25));
+let guesses = [
+  {
+    player1: 6,
+    player2: 2,
+  },
+  {
+    player1: 8,
+    player2: 3,
+  },
+  {
+    player1: 11,
+    player2: 25,
+  },
+];
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
